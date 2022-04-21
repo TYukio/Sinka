19617619e2aa506078eef7a8eae0c4955e2a8bd2
Luiz Krüger <46863600+itisluiz@users.noreply.github.com>
2022-04-21 09:31:12 -0700

    Estrutura do site

---
+++ sinka/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <link rel="stylesheet" href="/css/styles.css">
+    </head>
+
+    <body class="theme-light">
+
+        <h1>My First Heading</h1>
+
+        <p>My first paragraph.</p>
+
+    </body>
+</html>
