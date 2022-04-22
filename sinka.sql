d3412c41125887db00f2c073f2b6dae6073fb153
Luiz Krüger <46863600+itisluiz@users.noreply.github.com>
2022-04-21 23:54:49 -0700

    Criação da base de dados
    Criado arquivo .sql com as tabelas 'Person', 'PersonType' e 'Person_PersonType' para acomodar o registro de usuários.

---
+++ sinka.sql
@@ -0,0 +1,35 @@
+CREATE TABLE PersonType
+(
+	id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    title VARCHAR(32) NOT NULL,
+    details VARCHAR(255) NOT NULL,
+    icon VARCHAR(64) DEFAULT 'person' NOT NULL,
+
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE Person
+(
+	id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    login VARCHAR(32) NOT NULL,
+    pass VARCHAR(255) NOT NULL,
+    creation DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    birth DATE NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    first_name VARCHAR(32) NOT NULL,
+	last_name VARCHAR(32) NOT NULL,
+    biography VARCHAR(765) DEFAULT NULL,
+    
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE Person_PersonType
+(
+	id_person INT UNSIGNED NOT NULL,
+	id_persontype INT UNSIGNED NOT NULL,
+
+    FOREIGN KEY(id_person) REFERENCES Person(id),
+    FOREIGN KEY(id_persontype) REFERENCES PersonType(id),
+    
+	PRIMARY KEY(id_person, id_persontype)
+);
