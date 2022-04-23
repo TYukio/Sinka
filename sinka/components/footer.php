ea41109a1ada152feaeda8f556f41d623873ac6c
Luiz Krüger <46863600+itisluiz@users.noreply.github.com>
2022-04-23 01:06:19 -0700

    Footer da página
    Criado o componente compartilhado de rodapé.

---
+++ sinka/components/footer.php
@@ -0,0 +1,4 @@
+<footer>
+    <img class="logo" src="\img\sinka.svg"><br>
+    <p>Sinka® 2021. Talvez alguns direitos estejam reservados.</p>
+</footer>
--- sinka/index.php
+++ sinka/index.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html>
-
     <head>
         <?php include $_SERVER['DOCUMENT_ROOT'] . "/components/head.html";?>
     </head>
 
     <body theme="light">
-        <?php include $_SERVER['DOCUMENT_ROOT'] . "/components/navbar.php";?>
-        <h1 style="text-align: center;">Olá mundo</h1>
+        <div class="page">
+            <?php include $_SERVER['DOCUMENT_ROOT'] . "/components/navbar.php";?>
+            <h1 style="text-align: center;">Olá mundo</h1>
+            
+            <?php include $_SERVER['DOCUMENT_ROOT'] . "/components/footer.php";?>
+        </div>
     </body>
 </html>
