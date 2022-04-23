282449feeaf11dceb8da649f5dc354926538f742
Luiz <46863600+itisluiz@users.noreply.github.com>
2022-04-22 21:12:06 -0700

    Indentação corrigida
    Indentação agora usa somente tabs \t

--- sinka.sql
+++ sinka.sql
@@ -1,9 +1,9 @@
 CREATE TABLE PersonType
 (
-    id INT UNSIGNED AUTO_INCREMENT NOT NULL,
-    title VARCHAR(32) NOT NULL,
-    details VARCHAR(255) NOT NULL,
-    icon VARCHAR(64) DEFAULT 'person' NOT NULL,
+    	id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    	title VARCHAR(32) NOT NULL,
+	details VARCHAR(255) NOT NULL,
+	icon VARCHAR(64) DEFAULT 'person' NOT NULL,
 
 	PRIMARY KEY(id)
 );
@@ -11,14 +11,14 @@ CREATE TABLE PersonType
 CREATE TABLE Person
 (
 	id INT UNSIGNED AUTO_INCREMENT NOT NULL,
-    login VARCHAR(32) NOT NULL,
-    pass VARCHAR(255) NOT NULL,
-    creation DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    birth DATE NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    first_name VARCHAR(32) NOT NULL,
+	login VARCHAR(32) NOT NULL,
+	pass VARCHAR(255) NOT NULL,
+	creation DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	birth DATE NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	first_name VARCHAR(32) NOT NULL,
 	last_name VARCHAR(32) NOT NULL,
-    biography VARCHAR(765) DEFAULT NULL,
+	biography VARCHAR(765) DEFAULT NULL,
     
 	PRIMARY KEY(id)
 );
@@ -28,8 +28,8 @@ CREATE TABLE Person_PersonType
 	id_person INT UNSIGNED NOT NULL,
 	id_persontype INT UNSIGNED NOT NULL,
 
-    FOREIGN KEY(id_person) REFERENCES Person(id),
-    FOREIGN KEY(id_persontype) REFERENCES PersonType(id),
+    	FOREIGN KEY(id_person) REFERENCES Person(id),
+    	FOREIGN KEY(id_persontype) REFERENCES PersonType(id),
     
 	PRIMARY KEY(id_person, id_persontype)
 );
